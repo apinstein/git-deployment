apinstein/git-deployment

commit 21318eb454ff5330e625479e61bcf15f64577b91

Fix bug in last_tag_matching which was introduced by alpha-naming tag structure. updating to be strictly chronologically based.

lib/capistrano/gitflow.rb
@@ -11,16 +11,10 @@ module Capistrano
 
         namespace :gitflow do
           def last_tag_matching(pattern)
-            matching_tags = `git tag -l '#{pattern}'`.split
-            matching_tags.sort! do |a,b|
-              String.natcmp(b, a, true)
-            end
-
-            last_tag = if matching_tags.length > 0
-                         matching_tags[0]
-                       else
-                         nil
-                       end
+            # search for most recent (chronologically) tag matching the passed pattern, then get the name of that tag.
+            last_tag = `git describe --exact-match \`git log --tags='#{pattern}*' --format="%H" -1\``.chomp
+            return nil if last_tag == ''
+            return last_tag
           end
 
           def last_staging_tag()

commit c0ac6854b906f7c5a4875def63c035621a27a7fc

further bug fix to last_tag_matching to improve on changes introduced in 21318eb4.

lib/capistrano/gitflow.rb
@@ -12,7 +12,7 @@ module Capistrano
         namespace :gitflow do
           def last_tag_matching(pattern)
             # search for most recent (chronologically) tag matching the passed pattern, then get the name of that tag.
-            last_tag = `git describe --exact-match \`git log --tags='#{pattern}*' --format="%H" -1\``.chomp
+            last_tag = `git describe --exact-match --match '#{pattern}' \`git log --tags='#{pattern}*' --format="%H" -1\``.chomp
             return nil if last_tag == ''
             return last_tag
           end